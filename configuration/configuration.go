a7d84e1707dead236d347d616028a19f76c90525
snake <snake@snakes-MacBook-Pro.local>
2020-04-12 20:50:13 -0400

    first commit

---
+++ configuration/configuration.go
@@ -0,0 +1,51 @@
+package configuration
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+type Configuration struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+}
+
+func GetConfiguration() Configuration {
+	var c Configuration
+	file, err := os.Open("./config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&c)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c
+}
+
+func GetConnection() *gorm.DB {
+	c := GetConfiguration()
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		c.Host, c.Port, c.User, c.Password, c.Database)
+
+	db, err := gorm.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
