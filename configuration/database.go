8f8ec023715e163aad21c7d5b9a98c67915013df
snake <snake@snakes-MacBook-Pro.local>
2020-04-13 22:06:37 -0400

    feat: change database connection

---
+++ configuration/database.go
@@ -0,0 +1,57 @@
+package configuration
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/subosito/gotenv"
+)
+
+type database struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+}
+
+func getConfiguration() database {
+	var c database
+	file, err := os.Open("./.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	gotenv.Load()
+	fmt.Println(file)
+	c.Database = os.Getenv("DB_NAME")
+	c.Password = os.Getenv("DB_PASS")
+	c.Host = os.Getenv("DB_HOST")
+	c.Port = os.Getenv("DB_PORT")
+	c.User = os.Getenv("DB_USER")
+
+	fmt.Println(c.Database, c.Password, c.Host, c.Port, c.User)
+	if c.Database == "" || c.Password == "" || c.Host == "" || c.Port == "" || c.User == "" {
+		log.Fatal("env vars not defined")
+	}
+
+	return c
+}
+
+func GetConnection() *gorm.DB {
+	c := getConfiguration()
+	fmt.Println("new postgresql connection")
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
+		c.Host, c.Port, c.User, c.Database, c.Password)
+	db, err := gorm.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
